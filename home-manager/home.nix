8ef29a3fd4bbddc04ec7a181e4761b1193ff7282
joshuackeller <joshuackeller@gmail.com>
2026-08-26 20:34:06 -0700

    ghostty home-manager config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -38,23 +38,14 @@
     };
   };
 
+  programs.ghostty = {
+    enable = true;
+    package = pkgs.ghostty-bin;
+    enableZshIntegration = true;
+    settings = {
+      theme = "TokyoNight Moon";
+      font-family = "JetBrainsMonoNL Nerd Font Mono";
+      font-thicken = true;
+    };
+  };
 }
-
-# programs.ghostty = {
-#   enable = true;
-#   settings = {
-#     theme = "tokyonight";
-#     background = "#222436";
-#     fontFamily = "JetBrainsMonoNL Nerd Font Mono";
-#     fontThicken = true;
-#     fontSize = 12;
-#     adjustCellWidth = -10;
-#     adjustCellHeight = 2;
-#     macosIcon = "custom-style";
-#     macosIconFrame = "plastic";
-#     macosIconGhostColor = "ffffff";
-#     macosIconScreenColor = "000000";
-#     autoUpdate = "download";
-#     autoUpdateChannel = "stable";
-#   };
-# };
