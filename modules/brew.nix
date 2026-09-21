4119ab6c500e17a02bc6a40b9414e75b0bfa9b3d
joshuackeller <joshuackeller@gmail.com>
2026-09-21 12:55:43 -0700

    clean up extra apps

--- modules/brew.nix
+++ modules/brew.nix
@@ -4,10 +4,9 @@
     enable = true;
 
     onActivation = {
-      autoUpdate = true; # let Homebrew auto-update itself + formulae
-      upgrade = true; # upgrade installed formulae/casks during rebuild
-      cleanup = "zap"; # zap = aggressively remove anything not declared (most popular)
-      # alternatives: "uninstall" or "none"
+      autoUpdate = true;
+      upgrade = true;
+      cleanup = "zap";
     };
 
     taps = [
@@ -15,24 +14,11 @@
         name = "1password/tap";
         trusted = true;
       }
-      {
-        name = "nikitabobko/tap";
-        trusted = true;
-      }
-
     ];
 
     casks = [
       {
         name = "1password-cli";
-        trusted = true;
-      }
-      {
-        name = "aerospace";
-        trusted = true;
-      }
-      {
-        name = "ghostty";
         trusted = true;
       }
       {
